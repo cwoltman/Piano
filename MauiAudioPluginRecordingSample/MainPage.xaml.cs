f0cca29d7d99e2c919b5c48bb6c42d6aad5a116c
cgwol <cgwol@DESKTOP-3SFUCGS>
2024-04-01 18:42:02 -0500

    Gui Changes

--- MauiAudioPluginRecordingSample/MainPage.xaml.cs
+++ MauiAudioPluginRecordingSample/MainPage.xaml.cs
@@ -13,28 +13,38 @@ namespace MauiAudioPluginRecordingSample
 
             _audioManager = audioManager;
             _audioRecorder = audioManager.CreateRecorder();
+
+            // Set initial button text
+            UpdateButtonText();
         }
 
         private async void OnButtonClicked(object sender, EventArgs e)
         {
             if (await Permissions.RequestAsync<Permissions.Microphone>() != PermissionStatus.Granted)
-            { 
+            {
                 // TODO Inform your user
-                return; 
+                return;
             }
 
             if (!_audioRecorder.IsRecording)
             {
                 await _audioRecorder.StartAsync();
+                // Update button text when recording starts
+                UpdateButtonText();
             }
             else
             {
                 var recordedAudio = await _audioRecorder.StopAsync();
-
                 var player = AudioManager.Current.CreatePlayer(recordedAudio.GetAudioStream());
                 player.Play();
+                // Update button text when recording stops
+                UpdateButtonText();
             }
         }
-    }
 
+        private void UpdateButtonText()
+        {
+            CounterBtn.Text = _audioRecorder.IsRecording ? "Press to stop Recording" : "Press to Record";
+        }
+    }
 }
