cb04d49f2c2c526a472e4aa243a152a9ea0dcb7e
cgwol <cgwol@DESKTOP-3SFUCGS>
2024-04-01 18:16:13 -0500

    Initial Changes

---
+++ MauiAudioPluginRecordingSample/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Plugin.Maui.Audio;
+
+namespace MauiAudioPluginRecordingSample
+{
+    public partial class MainPage : ContentPage
+    {
+        readonly IAudioManager _audioManager;
+        readonly IAudioRecorder _audioRecorder;
+
+        public MainPage(IAudioManager audioManager)
+        {
+            InitializeComponent();
+
+            _audioManager = audioManager;
+            _audioRecorder = audioManager.CreateRecorder();
+        }
+
+        private async void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (await Permissions.RequestAsync<Permissions.Microphone>() != PermissionStatus.Granted)
+            { 
+                // TODO Inform your user
+                return; 
+            }
+
+            if (!_audioRecorder.IsRecording)
+            {
+                await _audioRecorder.StartAsync();
+            }
+            else
+            {
+                var recordedAudio = await _audioRecorder.StopAsync();
+
+                var player = AudioManager.Current.CreatePlayer(recordedAudio.GetAudioStream());
+                player.Play();
+            }
+        }
+    }
+
+}
